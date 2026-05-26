1bd6d59b55c8161fdecfa914b51b6e15236ce907
Abhinandan1403 <crazyabhi888@gmail.com>
2026-05-26 11:00:42 +0530

    Time: 195 ms (9.97%) | Memory: 22.6 MB (7.40%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,43 +1,21 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n = nums.size();
-        // vector<int> dp(n, -1);
-        // return f(nums, 0, dp);
-
-        int l = 0 , r = 0 , jumps = 0 ;
-
-        while(r<n-1){
-            int range = 0 ;
-            for(int i = l ; i<=r ; i++){
-                range = max(nums[i]+i, range);
-            }
-            jumps++;
-            l= r+1;
-            r = range;
-        }
-        return jumps;
+        vector<int>dp(nums.size(), -1);
+        return solve(nums, 0, dp);
     }
 
-    // int f(vector<int>& nums, int idx, vector<int>& dp){
-    //     int n = nums.size();
+    int solve(vector<int>& nums, int idx, vector<int>& dp){
+        if(idx >= nums.size()-1)return 0;
 
-    //     if(idx >= n-1) return 0;
+        if(dp[idx]!=-1)return dp[idx];
 
-    //     if(dp[idx] != -1) return dp[idx];
+        int mini = 1e8 ;
+        for(int i = 1 ; i<=nums[idx]; i++){
+            int steps = 1 + solve(nums, idx+i, dp);
+            mini = min(mini, steps);
+        }
 
-    //     int ans = INT_MAX;
-
-    //     for(int i = 1; i <= nums[idx]; i++){
-    //         if(idx + i < n){
-    //             int sub = f(nums, idx + i, dp);
-
-    //             if(sub != INT_MAX){
-    //                 ans = min(ans, 1 + sub);
-    //             }
-    //         }
-    //     }
-
-    //     return dp[idx] = ans;
-    // }
+        return dp[idx] = mini ;
+    }
 };
