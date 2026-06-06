7aeff14d6d11ae000bd171a6fb62f99e098fedfe
Abhinandan1403 <crazyabhi888@gmail.com>
2026-06-06 22:53:15 +0530

    Time: 21 ms (6.65%) | Memory: 19.4 MB (47.93%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,30 +1,42 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n = prerequisites.size();
         vector<vector<int>>adj(numCourses);
         vector<int>indegree(numCourses, 0);
         for(int i = 0 ; i<prerequisites.size() ; i++ ){
             indegree[prerequisites[i][0]]++;
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
-        queue<int>q;
-        vector<int>topo;
+
+        queue<int>q ;
         for(int i = 0 ; i<numCourses ; i++){
-            if(indegree[i]==0)q.push(i);
+            if(indegree[i] == 0){
+                q.push(i);
+            }
         }
+
+        int topo = 0 ;
+        vector<int>topov;
+
         while(!q.empty()){
             int node = q.front();
             q.pop();
-            topo.push_back(node);
+            topo++;
+            topov.push_back(node);
 
-            for(auto& it : adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0){
-                    q.push(it);
+            for(auto nbr : adj[node]){
+                indegree[nbr]--;
+
+                if(indegree[nbr] == 0){
+                    q.push(nbr);
                 }
             }
         }
-        return numCourses == topo.size();
+
+        for(auto it : topov){
+            cout << it << " " ;
+        }
+
+        return topo == numCourses ;
     }
 };
