46f31ac0dcea4260ccaa2629d54c17d4281bcd1e
Abhinandan1403 <crazyabhi888@gmail.com>
2026-06-18 11:35:09 +0530

    Time: 159 ms (6.39%) | Memory: 53.7 MB (35.73%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool isMatch(string& s, string& p) {
+        int m = s.size();
+        int n = p.size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(m - 1, n - 1, s, p, dp);
+    }
+
+    bool solve(int idx1, int idx2, string& s, string& p,
+               vector<vector<int>>& dp) {
+        if (idx1 < 0 && idx2 < 0)
+            return true;
+
+        if (idx2 < 0)
+            return false;
+
+        if (idx1 < 0) {
+            for (int i = 0; i <= idx2; i++) {
+                if (p[i] != '*')
+                    return false;
+            }
+            return true;
+        }
+
+        if (dp[idx1][idx2] != -1)
+            return dp[idx1][idx2];
+
+        bool ans = false;
+        if (s[idx1] == p[idx2]) {
+            ans = solve(idx1 - 1, idx2 - 1, s, p, dp);
+        }
+
+        else if (p[idx2] == '*') {
+            for (int i = 0; i <= idx1 + 1; i++) {
+                ans |= solve(idx1 - i, idx2 - 1, s, p, dp);
+            }
+        }
+
+        else if (p[idx2] == '?') {
+            ans = solve(idx1 - 1, idx2 - 1, s, p, dp);
+        }
+
+        else {
+            ans = false;
+        }
+
+        return dp[idx1][idx2] = ans;
+    }
+};
