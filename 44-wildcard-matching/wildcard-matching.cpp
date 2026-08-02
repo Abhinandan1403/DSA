29532f8063aab4e4c57745956ea070d1199c79b0
Abhinandan1403 <crazyabhi888@gmail.com>
2026-08-02 14:06:35 +0530

    Time: 46 ms (61.69%) | Memory: 55.2 MB (6.02%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,49 +1,41 @@
 class Solution {
 public:
-    bool isMatch(string& s, string& p) {
-        int m = s.size();
-        int n = p.size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return solve(m - 1, n - 1, s, p, dp);
+    bool isMatch(string s, string p) {
+        int n = s.size();
+        int m = p.size();
+        vector<vector<int>>dp(n+2, vector<int>(m+2, -1));
+        return solve(s, p, n - 1, m - 1, dp);
     }
 
-    bool solve(int idx1, int idx2, string& s, string& p,
-               vector<vector<int>>& dp) {
-        if (idx1 < 0 && idx2 < 0)
-            return true;
-
-        if (idx2 < 0)
+    bool solve(string& s, string& p, int n, int m, vector<vector<int>>& dp) {
+        if (m == -1 && n != -1)
             return false;
-
-        if (idx1 < 0) {
-            for (int i = 0; i <= idx2; i++) {
+        if (n == -1) {
+            for (int i = 0; i <= m; i++) {
                 if (p[i] != '*')
                     return false;
             }
             return true;
         }
 
-        if (dp[idx1][idx2] != -1)
-            return dp[idx1][idx2];
+        if(dp[n][m] != -1)return dp[n][m];
 
-        bool ans = false;
-        if (s[idx1] == p[idx2]) {
-            ans = solve(idx1 - 1, idx2 - 1, s, p, dp);
+        else if (p[m] == '?') {
+            dp[n][m] = solve(s, p, n - 1, m - 1, dp);
         }
 
-        else if (p[idx2] == '*') {
-            ans = solve(idx1, idx2 - 1, s, p, dp) ||
-                  solve(idx1 - 1, idx2, s, p, dp);
+        else if (p[m] == '*') {
+            dp[n][m] = solve(s, p, n-1, m, dp) || solve(s, p, n, m-1, dp);
         }
 
-        else if (p[idx2] == '?') {
-            ans = solve(idx1 - 1, idx2 - 1, s, p, dp);
+        else if(s[n] == p[m]){
+            dp[n][m] = solve(s, p, n-1, m-1, dp);
         }
 
-        else {
-            ans = false;
+        else{
+            dp[n][m] = false;
         }
 
-        return dp[idx1][idx2] = ans;
+        return dp[n][m];
     }
 };
