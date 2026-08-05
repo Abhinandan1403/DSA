947944223de7c02ed257b807972f09c6f278e9a6
Abhinandan1403 <crazyabhi888@gmail.com>
2026-08-05 16:09:59 +0530

    Time: 481 ms (24.24%) | Memory: 296.9 MB (6.83%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,54 +1,24 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
+        int prev = -1;
         int n = nums.size();
-        // vector<vector<int>>dp(n+2, vector<int>(n+2, 0));
-
-        // for(int idx = n-1 ; idx>=0 ; idx--){
-        //     for(int prev_idx = idx-1 ; prev_idx>=-1 ; prev_idx--){
-        //         int take = 0 ;
-        //         if(prev_idx == -1 || nums[prev_idx]<nums[idx]){
-        //             take = 1 + dp[idx+1][idx+1];
-        //         }
-
-        //         int notTake = dp[idx+1][prev_idx+1];
-
-        //         dp[idx][prev_idx+1] = max(take, notTake);
-        //     }
-        // }
-
-        // return dp[0][0];
-
-        vector<int>temp;
-        for(int i = 0 ; i<n ; i++){
-            if(temp.empty() || temp.back()<nums[i]){
-                temp.push_back(nums[i]);
-            }
-            else{
-                int idx = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
-                temp[idx]=nums[i];
-            }
-        }
-
-        return temp.size();
-
-
-
-        // return solve(nums, 0, -1, dp);
+        vector<vector<int>>dp(n+3, vector<int>(n+3, -1));
+        return solve(prev, 0, nums, dp);
     }
 
-    // int solve(vector<int>& nums, int idx, int prev_idx, vector<vector<int>>& dp){
-    //     if(idx == nums.size())return 0 ;
+    int solve(int prev, int idx, vector<int>& nums, vector<vector<int>>& dp){
 
-    //     if(dp[idx][prev_idx+1]!=-1)return dp[idx][prev_idx+1];
+        if(idx == nums.size())return 0;
 
-        // int take = 0 ;
-        // if(prev_idx == -1 || nums[prev_idx]<nums[idx]){
-        //     take = 1 + solve(nums, idx+1, idx, dp);
-        // }
+        if(dp[prev+1][idx]!=-1) return dp[prev+1][idx];
 
-        // int notTake = solve(nums, idx+1, prev_idx, dp);
+        int pick = INT_MIN;
+        if(prev == -1 || nums[prev]<nums[idx]){
+            pick = 1 + solve(idx, idx+1, nums, dp);
+        }
+        int notPick = solve(prev, idx+1, nums, dp);
 
-        // return dp[idx][prev_idx+1] = max(take, notTake);
-    // }
+        return dp[prev+1][idx] = max(pick, notPick);
+    }
 };
