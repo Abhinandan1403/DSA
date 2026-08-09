5b7f2e182ab6a98dcc672d5657bb6d10a035d9b3
Abhinandan1403 <crazyabhi888@gmail.com>
2026-08-09 15:40:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (43.67%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,37 +1,43 @@
 class Solution {
 public:
     int splitArray(vector<int>& nums, int k) {
-        int s = *max_element(nums.begin(), nums.end());
-        int e = accumulate(nums.begin(), nums.end(), 0);
-        int ans = -1 ;
+        int maxi = INT_MIN, sum = 0;
+        for (auto& num : nums) {
+            maxi = max(num, maxi);
+            sum += num;
+        }
 
-        while(s<=e){
-            int mid = s + (e-s)/2 ;
-            if(solve(nums, k, mid)){
-                ans = mid ;
-                e = mid - 1 ;
-            }
-            else{
-                s = mid+1 ;
+        if(sum == 0)return 0;
+
+        int s = maxi, e = sum;
+        int ans = 1e9;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (solve(nums, mid) <= k) {
+                ans = min(ans, mid);
+                e = mid - 1;
+            } else {
+                s = mid + 1;
             }
         }
         return ans;
     }
 
-    bool solve(vector<int>& nums, int k, int mid){
-        int grp = 0 ;
-        int sum = 0 ;
-        for(int i = 0 ; i<nums.size() ; i++){
-            if(sum+nums[i]<=mid){
-                sum = sum + nums[i] ;
+    int solve(vector<int>& nums, int m) {
+        int n = nums.size();
+        int grp = 0, sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+
+            if(sum == m){
+                grp++;
+                sum=0;
             }
-            else{
+            else if(sum > m){
                 grp++;
                 sum = nums[i];
             }
         }
-        grp++;
-
-        return grp > k ? false : true ;
+        return sum == 0 ? grp : ++grp;
     }
 };
