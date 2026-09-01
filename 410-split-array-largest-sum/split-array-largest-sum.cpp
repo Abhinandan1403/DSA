194718a66665e8ed6c8ba1aadffe91ca012cbc05
Abhinandan1403 <crazyabhi888@gmail.com>
2026-09-01 16:17:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (10.90%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,43 +1,46 @@
 class Solution {
-public:
-    int splitArray(vector<int>& nums, int k) {
-        int maxi = INT_MIN, sum = 0;
-        for (auto& num : nums) {
-            maxi = max(num, maxi);
-            sum += num;
-        }
-
-        if(sum == 0)return 0;
-
-        int s = maxi, e = sum;
-        int ans = 1e9;
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (solve(nums, mid) <= k) {
-                ans = min(ans, mid);
-                e = mid - 1;
-            } else {
-                s = mid + 1;
-            }
-        }
-        return ans;
-    }
-
-    int solve(vector<int>& nums, int m) {
-        int n = nums.size();
-        int grp = 0, sum = 0;
-        for (int i = 0; i < n; i++) {
+private:
+    int solve(vector<int>& nums, int k){
+        int grp = 0;
+        int sum = 0;
+        for(int i = 0 ; i<nums.size() ; i++){
             sum += nums[i];
-
-            if(sum == m){
+            if(sum == k){
                 grp++;
-                sum=0;
+                sum = 0;
             }
-            else if(sum > m){
+            else if(sum >k){
                 grp++;
                 sum = nums[i];
             }
         }
-        return sum == 0 ? grp : ++grp;
+        return sum == 0 ? grp : grp + 1;
+    }
+public:
+    int splitArray(vector<int>& nums, int k) {
+
+        if(k > nums.size())return -1;
+
+        int s = 0 , e = 0, n = nums.size();
+        for(int i = 0 ; i<n ; i++){
+            s = max(s, nums[i]);
+            e += nums[i];
+        }
+
+        int ans = 0;
+
+        while(s<=e){
+            int mid = s + (e-s)/2 ;
+            int grp = solve(nums, mid);
+
+            if(grp<=k){
+                ans = mid;
+                e = mid-1;
+            }
+            else{
+                s = mid + 1;
+            }
+        }
+        return ans;
     }
 };
